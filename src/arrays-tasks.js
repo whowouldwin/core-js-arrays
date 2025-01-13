08f27b494c78d04280005dd2b7b4134335485bf5
whowouldwin <leranetwork@gmail.com>
2025-01-13 23:18:55 +0300

    feat: implement getIdentityMatrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -433,8 +433,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, i) => {
+    return Array.from({ length: n }, (__, j) => (i === j ? 1 : 0));
+  });
 }
 
 /**
