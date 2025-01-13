f779414ac0d55a119729e04301b6fd2150388244
whowouldwin <leranetwork@gmail.com>
2025-01-13 23:29:09 +0300

    feat: implement getMaxItems

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -487,8 +487,14 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  if (arr.length === 0) {
+    return [];
+  }
+  return arr
+    .slice()
+    .sort((a, b) => b - a)
+    .slice(0, n);
 }
 
 /**
