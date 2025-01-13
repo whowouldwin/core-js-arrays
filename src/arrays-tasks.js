e28683160e0900eca3ae4fbe48595c79875f6b24
whowouldwin <leranetwork@gmail.com>
2025-01-13 21:37:47 +0300

    feat: implement getTail

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -210,8 +210,11 @@ function getHead(arr, n) {
  *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
  *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  if (n <= 0) {
+    return [];
+  }
+  return arr.slice(-n);
 }
 
 /**
