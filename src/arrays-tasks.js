431b49e88dfde6a6fae16dba14092d99c4d83cd8
whowouldwin <leranetwork@gmail.com>
2025-01-13 22:54:01 +0300

    feat: implement createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -277,8 +277,13 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n === 1) {
+    return Array(size).fill(0);
+  }
+  return Array(size)
+    .fill(null)
+    .map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
