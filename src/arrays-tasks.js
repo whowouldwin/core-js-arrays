807306ca87cad163a5184bcf0191d8820bab6ea7
whowouldwin <leranetwork@gmail.com>
2025-01-13 23:22:05 +0300

    feat: implement getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -450,8 +450,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((number, index) => (number % 2 !== 0 ? index : null))
+    .filter((index) => index !== null);
 }
 
 /**
