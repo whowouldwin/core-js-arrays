71b44b9cb2aa673e4771199a52bde8d6e4aa5764
whowouldwin <leranetwork@gmail.com>
2025-01-13 21:25:37 +0300

    feat: implement getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -127,8 +127,15 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((acc, currentElement) => {
+    return acc + currentElement;
+  }, 0);
+  const medium = sum / arr.length;
+  return parseFloat(medium.toFixed(2));
 }
 
 /**
