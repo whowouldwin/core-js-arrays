c65b684d3e22c7bf9182d837aa29a4747efb7b6d
whowouldwin <leranetwork@gmail.com>
2025-01-13 23:06:13 +0300

    feat: implement createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -349,8 +349,16 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  if (chunkSize <= 0) {
+    return [];
+  }
+  return arr.reduce((chunks, currentValue, index) => {
+    if (index % chunkSize === 0) {
+      chunks.push(arr.slice(index, index + chunkSize));
+    }
+    return chunks;
+  }, []);
 }
 
 /**
